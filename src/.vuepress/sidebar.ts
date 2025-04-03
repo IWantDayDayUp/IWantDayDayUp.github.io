a443193ed3b7ae812f5e3521bfaa992a44d72347
IWantDayDayUp <1462959616@qq.com>
2025-04-03 02:07:14 +0100

    💄 ui-tuning(sidebar.ts): Update sidebar
    1. Delete useless bar 2. Add new bar 3. Update bar info

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -2,25 +2,137 @@ import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
   "/": [
+    // home
     "",
+
+    // Demo
+    // {
+    //   text: "Demo",
+    //   icon: "laptop-code",
+    //   prefix: "demo/",
+    //   link: "demo/",
+    //   children: "structure",
+    // },
+
+    // EECS section
     {
-      text: "Demo",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
+      text: "📝 EECS",
+      // icon: "book",
+      prefix: "posts/EECS/",
+      // Whether current group is collapsible
+      collapsible: true,
+      // Whether current group is expanded by default
+      expanded: false,
+      // automatically generate sidebar items by useing the 'structure' option
       children: "structure",
     },
+
+    // Algorithm section
     {
-      text: "Articles",
-      icon: "book",
-      prefix: "posts/",
+      text: "🧠 Algorithm",
+      // icon: "book",
+      prefix: "posts/Algorithm/",
+      // Whether current group is collapsible
+      collapsible: true,
+      // Whether current group is expanded by default
+      expanded: true,
+      // automatically generate sidebar items by useing the 'structure' option
+      // children: "structure",
+      children: [
+        {
+          text: "👉 Template Index",
+          link: "Template/index",
+        },
+        {
+          text: "👉 Topic Index",
+          link: "Topic/index",
+        },
+        {
+          text: "👉 AtCoder",
+          // link: "AtCoder/",
+          // icon: "book",
+          prefix: "AtCoder/",
+          collapsible: true,
+          expanded: false,
+          children: "structure",
+        },
+        {
+          text: "👉 Codeforrces",
+          // link: "Codeforces/",
+          // icon: "book",
+          prefix: "Codeforces/",
+          collapsible: true,
+          expanded: false,
+          children: "structure",
+        },
+        {
+          text: "👉 All Templates",
+          // link: "Template/",
+          // icon: "book",
+          prefix: "Template/",
+          collapsible: true,
+          expanded: false,
+          children: "structure",
+        },
+        {
+          text: "👉 All Topics",
+          // link: "Topic/",
+          // icon: "book",
+          prefix: "Topic/",
+          collapsible: true,
+          expanded: false,
+          children: "structure",
+        },
+      ],
+    },
+
+    // Book section
+    {
+      text: "📚 Book",
+      // icon: "book",
+      prefix: "posts/Book/",
+      // Whether current group is collapsible
+      collapsible: true,
+      // Whether current group is expanded by default
+      expanded: false,
+      // automatically generate sidebar items by useing the 'structure' option
       children: "structure",
     },
+
+    // Project section
+    {
+      text: "🛠 Project",
+      // icon: "book",
+      prefix: "posts/Project/",
+      // Whether current group is collapsible
+      collapsible: true,
+      // Whether current group is expanded by default
+      expanded: false,
+      // automatically generate sidebar items by useing the 'structure' option
+      children: "structure",
+    },
+
+    // Vlog section
+    {
+      text: "📷 Vlog",
+      // icon: "book",
+      prefix: "posts/Vlog/",
+      // Whether current group is collapsible
+      collapsible: true,
+      // Whether current group is expanded by default
+      expanded: false,
+      // automatically generate sidebar items by useing the 'structure' option
+      children: "structure",
+    },
+
+    // Intro
     "intro",
-    {
-      text: "Slides",
-      icon: "person-chalkboard",
-      link: "https://ecosystem.vuejs.press/plugins/markdown/revealjs/demo.html",
-    },
+
+    // Slides
+    // {
+    //   text: "Slides",
+    //   icon: "person-chalkboard",
+    //   link: "https://ecosystem.vuejs.press/plugins/markdown/revealjs/demo.html",
+    // },
   ],
 });
